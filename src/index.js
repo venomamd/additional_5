b7a48b7fcc2721a00310b1c67f0cacde5f8b682b
venomamd <komashh@gmail.com>
2018-09-26 23:03:11 +0300

    task6 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  const config = bracketsConfig.join("").replace(/,/g, "");
+
+  for(let i = 0, strLength = str.length; i < strLength; i++) {
+    let char, indexOfTheChar, indexOfTheLast;
+    char = str[i];
+    indexOfTheChar = config.indexOf(char);
+    indexOfTheLast = stack.length - 1;
+
+    if(indexOfTheChar % 2 === 0) {
+      if(stack[indexOfTheLast] === config[indexOfTheChar + 1]) { 
+        stack.pop();
+      }else{ 
+        stack.push(char);
+      }         
+    }else if(indexOfTheChar % 2 !== 0) {
+      let indexInConfig = config.indexOf(stack[indexOfTheLast]);
+      if((indexInConfig + 1) === indexOfTheChar) {
+        stack.pop();
+      }else if(!stack.length) {
+        return false;
+      }
+    }      
+  }
+    return !stack.length;
 }
